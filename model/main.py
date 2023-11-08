3d43c37e25d7465eff67c06748774e13ef775366
Pratham <cse220001060@iiti.ac.in>
2023-11-08 22:54:53 +0530

    made new project

--- model/main.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from PIL import Image
-import os
-from deepface import DeepFace
-from Face_Detection import detect
-from Face_Recognition import recognize
-
-# detect
-recognize
--- server/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from model.models import Tokens, Classes, UserProfile, StudentData
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = '__all__'
